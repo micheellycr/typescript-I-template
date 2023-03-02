4997b0bab04b9f6a6da5d789aab250e34167a553
unknown <micheellycr@gmail.com>
2023-03-01 21:13:20 -0300

    resolucao-exercicios-barbosa-michelly-cristine

--- src/tipando/index.ts
+++ src/tipando/index.ts
@@ -1,11 +1,15 @@
-function buscarCarrosPorMarca(frota, marca) {
+import { TFrota, frota } from "./frota"
+
+function buscarCarrosPorMarca(frota: TFrota[], marca: string): TFrota[] {
   if (marca === undefined) {
     return frota
   }
 
   return frota.filter(
-    (carro) => {
+    (carro: TFrota) => {
       return carro.marca === marca
     }
   )
 }
+
+console.log(buscarCarrosPorMarca(frota, "Fiat"))
